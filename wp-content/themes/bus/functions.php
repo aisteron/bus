cec42ec557017489f9903157acd4562430508784
aisteron <timotheus@list.ru>
2017-08-01 23:30:11 +0300

    01z

--- wp-content/themes/bus/functions.php
+++ wp-content/themes/bus/functions.php
@@ -1,9 +1,9 @@
 <?php
-remove_action( 'wp_head', 'wp_resource_hints', 2 );
+remove_action('wp_head', 'wp_resource_hints', 2 );
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
-remove_action ('wp_head', 'rsd_link');
-remove_action( 'wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'wp_generator');
 
 function my_deregister_scripts(){
--- wp-content/themes/bus/header.php
+++ wp-content/themes/bus/header.php
@@ -2,9 +2,11 @@
 <html lang="ru">
 <head>
 	<meta charset="UTF-8">
+	<link rel='dns-prefetch' href='//fonts.googleapis.com'>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700&amp;subset=cyrillic-ext" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title></title>
 	
-	<?php wp_head();?>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php wp_head();?>	
 </head>
 <body>
