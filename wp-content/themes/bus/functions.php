10025a979a9497c5b11927ab58cb9b49a8b2fb4a
aisteron <timotheus@list.ru>
2017-08-01 18:05:52 +0300

    get rid unused code

---
+++ wp-content/themes/bus/functions.php
@@ -0,0 +1,12 @@
+<?php
+remove_action( 'wp_head', 'wp_resource_hints', 2 );
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action ('wp_head', 'rsd_link');
+remove_action( 'wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'wp_generator');
+
+function my_deregister_scripts(){
+  wp_deregister_script( 'wp-embed' );
+}
+add_action( 'wp_footer', 'my_deregister_scripts' );
